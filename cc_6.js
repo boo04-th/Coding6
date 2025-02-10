9e270c88e283549cf20f4df572785c4f15ae961e
Hient <hient@usf.edu>
2025-02-09 23:10:05 -0500

    Task 1 - Business Profit Calculation

--- cc_6.js
+++ cc_6.js
@@ -1,2 +1,8 @@
 //Task 1: Function Declaration
-
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    const profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`);
+    return profit;
+} // function for calculating total profits
+calculateProfit(20, 30, 100); // profit: $1000
+calculateProfit(50, 70, 200); // profit: $4000
