22c3924cde457972eb7c8aeddfc064b081c955b2
Hient <hient@usf.edu>
2025-02-09 23:15:00 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 } // function for calculating total profits
 calculateProfit(20, 30, 100); // profit: $1000
 calculateProfit(50, 70, 200); // profit: $4000
+
+//Task 2: Function Expression
+let calculateSalesTax = function(amount, taxRate) {
+    let salestaxes = amount * taxRate;
+    console.log(`Sales tax Computation: $${salestaxes}`);
+    }
+    
+    calculateSalesTax(100, 0.07); 
+    calculateSalesTax(500, 0.1);  
+    
