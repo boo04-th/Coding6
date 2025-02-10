5ed068917c48b92a73430cc61502f37ee6b4c97e
Hient <hient@usf.edu>
2025-02-09 23:18:47 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -16,3 +16,14 @@ let calculateSalesTax = function(amount, taxRate) {
     calculateSalesTax(100, 0.07); 
     calculateSalesTax(500, 0.1);  
     
+//Task 3: Arrow Function
+let calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage = 0; 
+    if(performanceRating === "Excellent")bonusPercentage = 0.20;
+    else if (performanceRating === "Good")bonusPercentage = 0.10;
+    else if (performanceRating === "Average")bonusPercentage = 0.05;
+    let bonus = salary * bonusPercentage;
+    console.log (`Bonus: $${bonus}`);
+}
+calculateBonus(5000, "Excellent"); // output should be 1000
+calculateBonus(7000, "Good");      // output should be 700
